cf0cc2d558e06145568d220a9b3c66af139f882a
Santiago_Dippolito <Santiago_Dippolito@ITRP1104>
2024-02-15 15:11:07 -0300

    Agregar archivos de proyecto.

---
+++ Models/DBInitializer.cs
@@ -0,0 +1,27 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", 
+                        LastName="Coronado", Password="123456"},
+                    new Client { Email = "santidipo@gmail.com", FirstName="Santiago",
+                        LastName="D Ippolito", Password="456283"},
+                    new Client { Email = "jacinfunes@gmail.com", FirstName="Jacinta",
+                        LastName="Funes", Password="283946"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
