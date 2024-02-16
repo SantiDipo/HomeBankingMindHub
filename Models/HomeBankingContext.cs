5132d152491c499d94fb4ab8650b94229b114dc4
Santiago Dippolito <Santiago_Dippolito@itrsa.com.ar>
2024-02-16 14:58:03 -0300

    final commit

--- Models/HomeBankingContext.cs
+++ Models/HomeBankingContext.cs
@@ -5,7 +5,7 @@ namespace HomeBankingMindHub.Models
     public class HomeBankingContext : DbContext
     {
         public HomeBankingContext(DbContextOptions<HomeBankingContext> options) : base(options) { }
-
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Account> Account { get; set; }
     }
 }
